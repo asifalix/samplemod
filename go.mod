73059b3d4dbcb36fdf622d0cc2e535bdfc754d0b
Asif <azifali@gmail.com>
2019-09-28 23:16:03 -0700

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gosamples/samplesmod
+
+go 1.13
---
+++ hello.go
@@ -0,0 +1,37 @@
+package hello
+
+func Hello() string {
+	return "Hello, world."
+}
+package hello
+
+import "rsc.io/quote/v3"
+
+func Hello() string {
+    return quote.HelloV3()
+}
+
+func Proverb() string {
+    return quote.Concurrency()
+}
+
+$ cat hello_test.go
+package hello
+
+import (
+    "testing"
+)
+
+func TestHello(t *testing.T) {
+    want := "Hello, world."
+    if got := Hello(); got != want {
+        t.Errorf("Hello() = %q, want %q", got, want)
+    }
+}
+
+func TestProverb(t *testing.T) {
+    want := "Concurrency is not parallelism."
+    if got := Proverb(); got != want {
+        t.Errorf("Proverb() = %q, want %q", got, want)
+    }
+}
